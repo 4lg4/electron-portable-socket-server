c918863a5272ca662c3267b82ca51c68020a3523
Alga <akgleal@gmail.com>
2020-05-27 19:35:32 +1000

    setup + tweaks

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,25 @@
 import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
 import App from "./app.vue";
 import router from "./router";
-import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
+
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSave, faPlus, faEdit, faTachometerAlt, faSlidersH, faTerminal, faStream } from '@fortawesome/free-solid-svg-icons'
+library.add(faSave, faPlus, faEdit, faTachometerAlt, faSlidersH, faTerminal, faStream);
+Vue.component("vue-fontawesome", FontAwesomeIcon);
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas"
+});
 
 /* eslint-disable no-new */
 window.App = new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
