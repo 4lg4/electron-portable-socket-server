ba81f0d3569167938dcad1c5fc61094c6283f419
Alga <akgleal@gmail.com>
2020-05-27 19:35:10 +1000

    split single module into multiple

--- frontend/src/modules/initial-module.vue
+++ frontend/src/modules/initial-module.vue
@@ -1,24 +1,51 @@
 <template>
   <div>
-    <p>you can access <strong>http://localhost:3333/ping</strong> on your browser</p>
-    <h1>Ping</h1>
-    <p>{{ message }}</p>
+    <p>
+      you can access <strong>http://localhost:3333/ping</strong> on your browser
+    </p>
+    <h1>Ping ???</h1>
+    <div>
+      <p>{{ message }}</p>
+    </div>
+    <div>
+
+
+<!--      <textarea v-model="body" class="a-textarea"></textarea> <br />-->
+      <button @click="send">Send</button>
+    </div>
   </div>
 </template>
 
 <script>
 import http from "axios";
 
+import { encode } from "@msgpack/msgpack";
+
 export default {
   data() {
     return {
-      message: ""
+      message: "",
+      body: ""
     };
   },
   created() {
     http.get("/api/ping").then(({ data }) => (this.message = data.message));
+  },
+  methods: {
+    send() {
+      console.log(this);
+      const a = JSON.parse(this.body);
+      console.log(a);
+      // console.log('decode', decode(a));
+      console.log('encode', encode(a));
+    },
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.a-textarea {
+  width: 80%;
+  height: 150px;
+}
+</style>
