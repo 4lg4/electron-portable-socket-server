43acbc724493cf02c0505c83fb28606943842e83
Alga <akgleal@gmail.com>
2020-05-27 19:33:29 +1000

    commands modules

---
+++ frontend/src/components/command-card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="command-card">
+    <h3>{{ value.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "command-card",
+  props: {
+    value: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<style scoped>
+.command-card {
+  border: 1px solid #000;
+}
+</style>
---
+++ frontend/src/modules/commands-module.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-button
+      type="is-info"
+      icon-right="plus"
+      class="commands-module__save"
+      pack="fas"
+      @click="add"
+    />
+
+    <h1>
+      Commands
+    </h1>
+    <div>
+      <command-card
+        v-for="(command, index) in storeState.commands"
+        :key="index"
+        :value="command"
+      ></command-card>
+    </div>
+  </div>
+</template>
+
+<script>
+// import http from "axios";
+// http.get("/api/ping").then(({ data }) => (this.message = data.message));
+import storeState from "@/store";
+import { reactive } from "@vue/composition-api";
+import CommandCard from "@/components/command-card.vue";
+
+export default {
+  components: {
+    "command-card": CommandCard
+  },
+  setup(props, context) {
+    const state = reactive({});
+
+    function add() {
+      context.root.$router.push({ path: "/commands/new" });
+    }
+
+    return {
+      state,
+      storeState,
+      add
+    };
+  }
+};
+</script>
+
+<style scoped>
+.commands-module__save {
+  position: absolute;
+  right: 15px;
+  top: 15px;
+}
+</style>
